6bbe8ab2830e9692fbcfed13c28d60d5dcc12101
Benjamin <bwinston@fitbit.com>
2015-04-25 13:09:21 -0700

    all in a days work! Today I did tests with directed sound and interference, managed to get polling working and a wall of sensing!

---
+++ fs/root/attemptPolling.js
@@ -0,0 +1,39 @@
+// node script for development purposes
+
+var b = require('bonescript');
+
+var pins = ["P9_11", "P9_12"];
+
+pins.forEach(function (pin) {
+	if (b.pinMode(pin, b.OUTPUT)) {
+		console.log("able to set pin mode, initializing to zero");
+		b.digitalWrite(pin, b.LOW);
+	} else {
+		console.log("unable to set pin mode");
+	}
+
+	b.getPinMode(pin, printPinData);
+});
+
+function printPinData(x) {
+	console.log("completed setting pin mode");
+    console.log('mux = ' + x.mux);
+    console.log('pullup = ' + x.pullup);
+    console.log('slew = ' + x.slew);
+    console.log('options = ' + x.options.join(','));
+    console.log('err = ' + x.err);
+}
+
+function pollPin1() {
+	b.digitalWrite(pins[1], b.HIGH);
+	setTimeout(function () {b.digitalWrite(pins[1], b.LOW)}, 1);
+	setTimeout(pollPin0,5);	
+}
+
+function pollPin0() {
+	b.digitalWrite(pins[0], b.HIGH);
+	setTimeout(function () {b.digitalWrite(pins[0], b.LOW)}, 1);
+	setTimeout(pollPin1,5);
+}
+
+pollPin0(); // kick it all off
