6f70b44ed54f5f14acb9800243b0ac34d2e602cd
Benjamin Winston <bwinston@sfo-m-3.local>
2015-07-03 19:56:36 -0700

    tied in beaglebone to center of hula hoop mount for prototype and updated the port assignments and improved code readability

--- fs/root/attemptPolling.js
+++ fs/root/attemptPolling.js
@@ -2,14 +2,36 @@
 
 var b = require('bonescript');
 
-var pins = ["P9_11", "P9_12"];
+// we poll the devices around in a ring.
 
-pins.forEach(function (pin) {
+var pollingControlPins = ["P9_11", "P9_12", "P9_13", "P9_14"];
+var pollingReadDataPins = ["P8_07", "P8_08", "P8_09", "P8_10"];
+
+// set up the sensor control ports by setting them to low
+//  - they later go high to ask the sensors to provide accurate data
+//  - the sensor data must be read from pollingReadDataPins whilst 
+//    the control pin for that sensor is high
+
+pollingControlPins.forEach(function (pin) {
 	if (b.pinMode(pin, b.OUTPUT)) {
-		console.log("able to set pin mode, initializing to zero");
+		console.log("able to set pollingControlPin mode, initializing to zero");
 		b.digitalWrite(pin, b.LOW);
 	} else {
-		console.log("unable to set pin mode");
+		console.log("unable to set pollingControlPin mode");
+	}
+
+	b.getPinMode(pin, printPinData);
+});
+
+// set up the PWM input pins from the sensors
+// a sensible value for that sensor is only available
+// when the pollingControlPin for that sensor is high
+
+pollingReadDataPins.forEach(function (pin)) {
+	if (b.pinMode(pin, b.INPUT)) {
+		console.log("able to set pollingReadDataPin mode");
+	} else {
+		console.log("unable to set pollingReadDataPin mode");
 	}
 
 	b.getPinMode(pin, printPinData);
@@ -24,16 +46,32 @@ function printPinData(x) {
 	console.log('err = ' + x.err);
 }
 
-function pollPin1() {
+function pollSensor3() {
+	b.digitalWrite(pins[3], b.HIGH);
+	// TODO: read sensor3
+	setTimeout(function() {b.digitalWrite(pings[3], b.LOW)}, 1);
+	setTimeout(pollSensor0,5);
+}
+
+function pollSensor2() {
+	b.digitalWrite(pins[2], b.HIGH);
+	// TODO: read sensor2
+	setTimeout(function() {b.digitalWrite(pings[2], b.LOW)}, 1);
+	setTimeout(pollSensor3,5);
+}
+
+function pollSensor1() {
 	b.digitalWrite(pins[1], b.HIGH);
+	// TODO: read sensor1
 	setTimeout(function () {b.digitalWrite(pins[1], b.LOW)}, 1);
-	setTimeout(pollPin0,5);	
+	setTimeout(pollSensor2,5);	
 }
 
-function pollPin0() {
+function pollSensor0() {
 	b.digitalWrite(pins[0], b.HIGH);
+	// TODO: read sensor0
 	setTimeout(function () {b.digitalWrite(pins[0], b.LOW)}, 1);
-	setTimeout(pollPin1,5);
+	setTimeout(pollSensor1,5);
 }
 
-pollPin0(); // kick it all off
+pollSensor0(); // kick it all off
