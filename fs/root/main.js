d21def3382f3e62b8e5dd101cb7e74f5b05befc9
Benjamin Winston <bwinston@fitbit.com>
2016-06-19 20:23:32 -0700

    Major rewrite to include object-oriented javascript and this one seems to work well.

--- fs/root/main.js
+++ fs/root/main.js
@@ -1,101 +1,134 @@
-// main polling loop
+// main sensors application
 
-// read command-line arguments..
-
+var net = require('net');
 var getopt = require('node-getopt');
+var sensor = require('sensor.js');
+var config = require('./sensors-config.json');
+
+var SMOOTHER_SIZE = 50;
+
+// read command-line arguments
+
 opt = getopt.create([
-	['' , 'host=HOST'     , 'the host running OSC to MIDI bridge.'],
-	['' , 'port=[PORT]'   , 'the UDP port for the bridge on that host (8000 is the default).'],
-	['' , 'debug=[SENSOR]' , 'dump the raw data from sensor with number SENSOR (0-5) to the console.'],
+	['' , 'osc_host=HOST'     , 'the host running OSC to MIDI bridge.'],
+	['' , 'osc_port=[PORT]'   , 'the UDP port for the bridge on that host (8000 is the default).'],
+	['' , 'debug=IP' , 'dump the raw data from sensor with ip IP to the console.'],
+	['' , 'port=[PORT]' , 'the UDP port that this program will listen on (8234 is the default)'],
 	['h' , 'help'         , 'display this help'],
 	['v' , 'version'      , 'show version']
 ]).bindHelp();
 var options = opt.parseSystem().options; // parse command line
 
-if (options['host'] == null) {
+// required options
+
+if (options['osc_host'] == null) {
  	console.error(opt.getHelp());
  	process.exit();
 }
+
+// defaults
+
+if (options['osc_port'] == null) {
+	options['osc_port'] = 8000;
+}
 if (options['port'] == null) {
-	options['port'] = 8000;
+	options['port'] = 8234;
 }
 
-var debug = null; // which IP to output to console.
-
-o = options['debug']
-
-var net = require('net'); // we'll be needing this
-
-if (o != null) {
-	if (net.isIPv4(o)) {
-		debug = o;
-	} else {
+if (options['debug'] != null) {
+	if (!net.isIPv4(options['debug'])) {
 	 	console.error("Invalid sensor IP." + opt.getHelp());
 	 	process.exit();	
 	}
 }
 
-console.log("I will be sending OSC to host " + options['host'] + " on UDP port " + options['port']);
-console.log("I will be listening for sensor UDP data on all IPs, on UDP port 31337.");
+// options to variables
 
+var osc_host = options['osc_host'];
+var osc_port = options['osc_port'];
+var port = options['port'];
+var debug = options['debug'];
+
+console.log("I will be sending OSC to host " + osc_host + " on UDP port " + osc_port);
+console.log("I will be listening for sensor UDP data from all IPs, on UDP port " + port);
+
+// load the sensor config
+
+var sensors = []; // array of sensor objects
+
+config.sensors.forEach(function(s) {
+	sensors.push(sensor.createSensor(s.ip, SMOOTHER_SIZE, osc_host, osc_port));
+});
+
+// Receiving sensor UDP traffic...
 
 /*
+we only get values sent to us in response to taking their sense pins high,
+so we're constantly ready to accept any value from any sensor.
 
-Receiving sensor UDP traffic...
-
+we then poll each of the configured sensors for a value
 */
 
-// hopefully the value from a sensor only changes when
-// we take it's control pin high!
-//
-// otherwise just accepting every value we get isn't going to work:
-
-
-// we receive from anywhere, accomodating new sensors with a new smoother object
-// and a new place to store a previous value:
-
-var sensors = []; // associative array of IP addresses to previous values
-var sensor = require('sensor.js');
+// start up the network receiver
 
 const dgram = require('dgram');
 const server = dgram.createSocket('udp4');
 
+function decodeDistanceValue(serialData) {
+	// extract the serial distance in inches from the received packet,
+	// e.g.: <Buffer 00 01 ff fe 52 30 30 39 0d>
+	return parseInt(serialData.slice(5, 8).toString()); // magic output format from ultrasonics
+}
+
 server.on('message', (msg, rinfo) => { // received UDP packet
-  
-  console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+	for (var i=0;i<sensors.length;i++) {
+		var s = sensors[i];
 
-  if (sensors[rinfo.address] == undefined) {  // the first we're hearing from this sensor
-    sensors[rinfo.address] = sensor.newSensor(); // make a new sensor object for it
-  }
+		if (s.ip == rinfo.address.toString()) {
+			s.pushInput(decodeDistanceValue(msg));
+			return;
+		}
+	}
 
-  s = sensors[rinfo.address];
-
-  // not sure what I've got to do here - do I need to stream reconstruct the serial data? Does it come one character at a time?
-
+	console.log(`Received packet from unknown sensor: ${rinfo.address} on port ${rinfo.port}`);
 });
 
 server.on('listening', () => {
-  var address = server.address();
-  console.log(`server listening ${address.address}:${address.port}`);
+	var address = server.address();
+	console.log(`Server listening ${address.address}:${address.port}`);
 });
 
 server.on('error', (err) => {
-  console.log(`server error:\n${err.stack}`);
-  server.close();
-  process.exit(1);
+	console.log(`server error:\n${err.stack}`);
+	server.close();
+	process.exit(1);
 });
 
+// bind
 
-server.bind(31337);
+server.bind(port, function() {
+	poll();
+});
 
+// poll
 
-var osc = require('osc.js');
-osc.connect(options['host'], options['port']);
+var index = 0;
 
+function sendPollPacket() {
+	var s = sensors[index++];
 
-// we poll the devices by taking the control pins high in a ring, and reading changes
-// in the values we receive over UDP.
+	trigger = new Buffer(1).fill(1);
+	offset_into_array = 0;
+	bytes_to_send = 1;
 
-for (var sensor in sensors) {
-	console.log("polling sensor on IP " + sensor);
+	server.send(trigger, offset_into_array, bytes_to_send, 20108, s.ip, function() {
+		if (index >= sensors.length) {
+			index = 0;
+		}
+		setTimeout(sendPollPacket, 100);
+	});
+}
+
+function poll() {
+	sendPollPacket();
 }
