defb2ccff49c52a9005d31f49822ffcc7ca776da
Benjamin Winston <bwinston@fitbit.com>
2016-07-23 15:40:22 -0700

    Updated install script to install 'forever' to run node scripts

--- fs/root/install.sh
+++ fs/root/install.sh
@@ -10,7 +10,12 @@ systemctl disable node-red.service
 systemctl stop node-red.service
 systemctl disable wpa_supplicant.service
 systemctl stop wpa_supplicant.service
+killall nodejs
 
 echo Installing node\'s \"forever\" package
 
+apt-get update
+apt-get dist-upgrade
+
 npm install forever -g
+
